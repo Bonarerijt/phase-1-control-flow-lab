bb52ece90ccd7caae07100851d03039e6655d522
Bonarerijt <prudyjj4@gmail.com>
2025-09-07 06:52:59 +0300

    Practiced on writing if...else if...else statements,ternary operations and switch statements.

--- index.js
+++ index.js
@@ -1,11 +1,35 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(ride) {
+  let message;
+  if (ride <= 400) {
+    message = 'This one is on me!';
+  }
+    else if(ride > 400 && ride <= 2000) {
+      message = 'That will be twenty bucks.';
+    }
+    else if (ride >2000 && ride <= 2500){
+      message = 'I will gladly take your thirty bucks.';
+    }
+    else {
+      message = 'No can do.';
+    }
+  return message;
 }
+console.log(scuberGreetingForFeet(2501));
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function ternaryCheckCity(cityName) { 
+  return cityName ==='NYC'?"Ok, sounds good.":'No go.';
 }
+console.log(ternaryCheckCity('NYC'));
+
+function switchOnCharmFromTip(amountOfTip) {
+  switch (amountOfTip) {
+    case 'generous':
+      return 'Thank you so much.';
+    case 'not as generous':
+      return 'Thank you.';
+    default:
+      return 'Bye.'
+  }
+}
+console.log(switchOnCharmFromTip('generous'));
